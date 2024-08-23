7d13d1610f245a52d7e382eaab1f544b03ce3df3
Marco De Toma <marco.de.toma@fastned.nl>
2024-08-23 08:40:38 +0200

    doc: fix documentation

--- fixture-generex/build.gradle.kts
+++ fixture-generex/build.gradle.kts
@@ -1,6 +1,6 @@
 /*
  * Copyright 2021-2023 Appmattus Limited
- *           2024 Detomarco Limited
+ *           2024 Detomarco
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
