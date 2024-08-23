80859181d012b8c542f899d6b2886f16ccd5b3f8
Marco De Toma <marco.de.toma@fastned.nl>
2024-08-23 06:44:49 +0200

    feat: add fixture-generex module

---
+++ fixture-generex/build.gradle.kts
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2021-2023 Appmattus Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm")
+}
+
+val mockitoKotlinVersion: String by project
+val kotlinVersion: String by project
+val kotlinxVersion: String by project
+val junit4Version: String by project
+val generexVersion: String by project
+
+dependencies {
+    api(kotlin("stdlib-jdk8"))
+    api(project(":fixture"))
+    api("com.github.mifmif:generex:$generexVersion")
+
+    testImplementation("junit:junit:$junit4Version")
+    testImplementation(kotlin("test"))
+    testImplementation(kotlin("test-junit"))
+    testImplementation("org.mockito.kotlin:mockito-kotlin:$mockitoKotlinVersion")
+
+    testImplementation(kotlin("reflect"))
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:$kotlinxVersion")
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+    withSourcesJar()
+    withJavadocJar()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = JavaVersion.VERSION_17.toString()
+}
+
+tasks.named("check") {
+    finalizedBy(rootProject.tasks.named("detekt"))
+}
